2aed8ecf38a30793504cdd407fccbc3f2ca32f63
Wangari Samantha <samanthanyams5@gmail.com>
2023-07-10 12:33:35 +0300

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'hero_powers/create'
-  get 'powers/index'
-  get 'powers/show'
-  get 'powers/update'
-  get 'heroes/index'
-  get 'heroes/show'
   resources :heroes, only: [:index, :show]
   resources :powers, only: [:index, :show, :update]
   resources :hero_powers, only: [:create]
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,15 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# Create heroes
-hero1 = Hero.create(name: 'Kamala Khan', super_name: 'Ms. Marvel')
-hero2 = Hero.create(name: 'Doreen Green', super_name: 'Squirrel Girl')
-hero3 = Hero.create(name: 'Gwen Stacy', super_name: 'Spider-Gwen')
 
-# Create powers
-power1 = Power.create(name: 'super strength', description: 'gives the wielder super-human strengths')
-power2 = Power.create(name: 'flight', description: 'gives the wielder the ability to fly through the skies at supersonic speed')
+hero1 = Hero.create(name: "Steve Rogers", super_name: "Captain America")
+hero2 = Hero.create(name: "Brie Larson", super_name: "Captain Marvel")
+hero3 = Hero.create(name: "Benedict Cumberbatch", super_name: "Dr Strange")
 
-# Create hero powers
-hero_power1 = HeroPower.create(hero: hero1, power: power1, strength: 'Strong')
-hero_power2 = HeroPower.create(hero: hero1, power: power2, strength: 'Weak')
-hero_power3 = HeroPower.create(hero: hero2, power: power1, strength: 'Average')
-hero_power4 = HeroPower.create(hero: hero3, power: power1, strength: 'Strong')
-hero_power5 = HeroPower.create(hero: hero3, power: power2, strength: 'Average')
+power1 = Power.create(name: "Super Strength", description: "Gives the wielder super-human strengths")
+power2 = Power.create(name: "Flight", description: "Gives the wielder ability to fly through skies at supersonic speed")
+power3 = Power.create(name: "Super Senses", description: "Gives the wielder ability to sense danger from afar")
+
+hero_power1 = HeroPower.create(hero: hero1, power: power1, strength: "Strong")
+hero_power2 = HeroPower.create(hero: hero1, power: power3, strength: "Weak")
+hero_power3 = HeroPower.create(hero: hero2, power: power2, strength: "Average")
